542545c42876ee21c2d31fc37111e028e02e1def
CshardZ <vivekhubstrt@gmail.com>
2025-02-13 17:56:19 +0530

    Design(update): add dynamic message-block building

--- app.js
+++ app.js
@@ -10,3 +10,37 @@ function setActivePage(clickedElement) {
 
     clickedElement.parentElement.classList.add("active")
 }
+
+
+
+let userInputText;
+let textArea = document.getElementById('text-area');
+textArea.addEventListener('keydown', function(event) {
+    if(event.key === "Enter" && !event.shiftKey) {
+        event.preventDefault();
+
+        sendUserInput();
+    }
+})
+
+function sendUserInput() {
+    userInputText = textArea.value;
+    textArea.value = "";
+    if (userInputText.trim()) {
+        
+        let messageBlock = document.createElement('div');
+        messageBlock.classList.add('message-block');
+
+        let userInput = document.createElement('p');
+        userInput.id = 'user-input';
+        userInput.textContent = userInputText;
+
+        let hr = document.createElement('hr');
+
+        messageBlock.appendChild(userInput);
+        messageBlock.appendChild(hr);
+
+        let allMessages = document.getElementById('all-messages');
+        allMessages.appendChild(messageBlock);
+    }
+}
